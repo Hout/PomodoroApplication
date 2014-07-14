bb9079ba7c3d0d9cc475f21b8417f71294ef5b8f
Brad Hammond <bradhammond@Brads-MacBook-Pro.local>
2014-07-14 11:33:42 -0400

    Initial Commit

---
+++ Pomodoro/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pomodoro
+//
+//  Created by Brad Hammond on 7/14/14.
+//  Copyright (c) 2014 Brad Hammond. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
